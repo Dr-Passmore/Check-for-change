a939317b86633a830a951aa55da0e763574f3c0a
Dr-Passmore <phillipspassmore@gmail.com>
2023-06-18 19:03:17 +0100

    Timestamp now updating as expected

--- main.py
+++ main.py
@@ -4,20 +4,20 @@ import csv
 import time
 from datetime import datetime, timedelta
 class CheckForChange():
-    def __init__(self, website, targetPhrase):
+    def __init__(self, website, targetPhrase, timestamp):
         request = requests.get(website)
         fileName = f"websiteContent/{website.replace('/', '_')}.txt"
         storageDirectory = "websiteContent"
         if not os.path.exists(storageDirectory):
             os.makedirs(storageDirectory)
         if os.path.isfile(fileName):
-            CheckForChange.compareWebsite(self, fileName, request, targetPhrase)
+            CheckForChange.compareWebsite(self, fileName, request, targetPhrase, timestamp)
         else:
             with open(fileName, 'w') as file:
                 file.write(request.content.decode()) 
                 time.sleep(20)
 
-    def compareWebsite(self, fileName, request, targetPhrase):
+    def compareWebsite(self, fileName, request, targetPhrase, timestamp):
         with open(fileName, "r") as file:
             savedVersion = file.read()
         if request.content.decode() == savedVersion:
@@ -28,41 +28,37 @@ class CheckForChange():
             print("New content")
             with open(fileName, 'w') as file:
                 file.write(request.content.decode()) 
-            if targetPhrase in request.content.decode(): 
-                CheckForChange.alert(self)
+            if targetPhrase in request.content.decode():
+                #check timestamp if within 24 hours do not alert
+                timestamp_str = timestamp
+                print(timestamp_str)
+                print(timestamp)
+                timestamp = datetime.strptime(timestamp_str, "%Y-%m-%d %H:%M:%S")
+
+                current_time = datetime.now()
+
+                if current_time - timestamp < timedelta(hours=24):
+                    # The timestamp is within the last 24 hours
+                    print("Timestamp is within the last 24 hours.")
+                    time.sleep(20)
+                else:
+                    print("Timestamp is older than 24 hours.") 
+                    # Create new timestamp
+                    new_timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                    # Update the timestamp in the current row
+                    row[2] = new_timestamp
+
+                    # Write the updated rows back to the CSV file
+                    with open("targets.csv", "w", newline='') as file:
+                        writer = csv.writer(file, delimiter=",")
+                        writer.writerows(rows)
+                    CheckForChange.alert(self)
             else:
                 time.sleep(20)
 
     def alert(self):
         print("test")
         time.sleep(20)
-'''
-with open("targets.csv", "r") as f:
-    reader = csv.reader(f, delimiter=",")
-    for row in reader:
-        website = row[0].strip('[]"')
-        targetPhrase = row[1].strip('[]"')
-        if len(row) < 3:
-            # Get the current timestamp
-            current_time = datetime.now()
-
-            # Subtract 24 hours from the current timestamp
-            time_24_hours_ago = current_time - timedelta(hours=24)
-
-            # Convert the timestamps to string format
-            time_24_hours_ago_str = time_24_hours_ago.strftime("%Y-%m-%d %H:%M:%S")
-            row.insert(2, time_24_hours_ago_str)
-            
-        else:
-            timestamp = row[2].strip('[]"')
-
-        
-        CheckForChange(website, targetPhrase)
-
-    with open("targets.csv", "w", newline='') as file:
-            writer = csv.writer(file, delimiter=",")
-            writer.writerows(rows)
-'''
 
 rows = []  # Initialize an empty list to store rows
 
@@ -94,4 +90,4 @@ for row in rows:
     website = row[0].strip('[]"')
     targetPhrase = row[1].strip('[]"')
     timestamp = row[2].strip('[]"')
-    CheckForChange(website, targetPhrase)
+    CheckForChange(website, targetPhrase, timestamp)
